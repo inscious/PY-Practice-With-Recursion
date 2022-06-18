8ab0b9577c934df037a61e7714d8b9d3aeb6098b
Saul Cortes <92839820+inscious@users.noreply.github.com>
2022-06-18 13:18:17 -0700

    sol1

--- solution1.py
+++ solution1.py
@@ -1,11 +1,7 @@
 # Write code for algorithm 1 below
 def count_down(n):
-
-    # Base case
     if n == 0:
         return
-
-    # Recursive case
     else:
         print(n)
         count_down(n - 1)
